c97b87471ba312b3ea27c8a509432f34e82f0aef
Ramil Safin <RamilSafNab1996@gmail.com>
2021-03-26 19:32:57 +0300

    Configure project

---
+++ dataset/generate_csv_dataset.cpp
@@ -0,0 +1,55 @@
+#include <fstream>      // ifstream, ofstream
+#include <iostream>     // cout
+#include <sstream>      // stringstream
+#include <string>       // string, getline
+#include <string_view>  // string_view
+#include <random>       // mt19937_64, random_device
+
+using namespace std;
+
+// абсолютный путь до набора данных (константа времени компиляции)
+static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
+
+int main(int argc, char** argv) {
+
+  // можете передать путь до входного/выходного файла в качестве аргумента (т.е. не обязательно использовать kDatasetPath)
+  for (int index = 0; index < argc; index++) {
+    cout << "Arg: " << argv[index] << '\n';
+  }
+
+  const auto path = string{kDatasetPath};  // конвертация string_view в string
+  cout << "Dataset path: " << path << endl;
+
+  // чтения набора данных (пример)
+  auto input_stream = ifstream{path + "/dataset-example.csv"};
+
+  if (input_stream) {
+    int line_number = 0;
+
+    // построчное чтение
+    for (string line; getline(input_stream, line); /* ... */) {
+      stringstream ss{line};  // оборачиваем строку в объект "поток"
+
+      for (string token; getline(ss, token, ','); /* ... */) {
+        cout << "Token: [" << token << "] at line " << line_number << '\n';
+      }
+
+      cout << line << '\n';
+      line_number++;
+    }
+  }
+
+  // генерация набора данных (пример)
+  auto output_stream = ofstream{path + "/dataset-generated.csv", ios::ios_base::app};
+  auto engine = mt19937_64{random_device{}()};
+  auto dist = uniform_int_distribution(0, 10);
+
+  if (output_stream) {
+   for (int counter = 0; counter < 10; counter++) {
+     output_stream << dist(engine) << ',';
+   }
+    output_stream << dist(engine) << '\n';
+  }
+
+  return 0;
+}
