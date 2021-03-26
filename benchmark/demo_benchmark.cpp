6467ca445f072c6a155e356d960f99810dee65aa
Ramil Safin <RamilSafNab1996@gmail.com>
2021-03-26 17:47:27 +0300

    Configure project

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string_view>
 
 #include "data_structure.hpp"
--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
 
 using namespace std;
 
-class S {
-private:
-  int a = 0;
-};
-
-int main(int argc, char **argv) {
-  cout << "Hello, stranger!" << endl; // fd f
-  return 0;                           // fd f
+int main(int argc, char *argv[]) {
+  for (int index = 0; index < argc; index++) {
+    cout << index << ": " << argv[index] << endl;
+  }
+  return 0;
 }
