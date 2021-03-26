dd5d632b9c924357c7f5c947576ea54a4c258e24
Ramil Safin <RamilSafNab1996@gmail.com>
2021-03-26 14:44:38 +0300

    Configure project

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 
 #include "data_structure.hpp"
 
+using namespace std;
+
+class S {
+private:
+  int a = 0;
+};
+
 int main(int argc, char **argv) {
-  std::cout << "Hello, stranger!" << std::endl;
-  return 0;
+  cout << "Hello, stranger!" << endl; // fd f
+  return 0;                           // fd f
 }
